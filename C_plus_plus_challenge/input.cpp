e0a8a1cb3e9efa5003a51a1538ff2ff50c5ae7b3
xianling zhang <xianlingzhang6@gmail.com>
2016-03-04 21:43:45 -0500

    Create input.cpp

---
+++ C_plus_plus_challenge/input.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+   int a=1;
+   int b=2;
+   int c=7;
+   
+   cin>>c>>b>>c;
+   
+   int sum;
+   
+   sum=a+b+c;
+   
+   cout<<sum;
+   
+   retun(0);
+}
