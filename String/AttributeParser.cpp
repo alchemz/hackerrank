04d131abf1556be08eab7fe2a2e3bd3d52e65cd5
xianling zhang <xianlingzhang6@gmail.com>
2016-03-06 20:36:15 -0500

    Create AttributeParser.cpp

---
+++ String/AttributeParser.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    <tag1 value = "HelloWorld">
+    <tag2 name = "Name1">
+    </tag2>
+</tag1>
+    return 0;
+}
