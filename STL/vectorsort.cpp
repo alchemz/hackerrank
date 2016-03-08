3e36c0012c4b2dc5b8318e4aeb3d3b7fd8a3fc61
xianling zhang <xianlingzhang6@gmail.com>
2016-03-08 18:27:59 -0500

    Update vectorsort.cpp

--- STL/vectorsort.cpp
+++ STL/vectorsort.cpp
@@ -22,19 +22,25 @@ sort(v.begin(), v.end());
 #include <algorithm>
 using namespace std;
 
-int main(){
-vector<int> v;
-int size;
-cin>>size;
-int a;
-for(int i=0; i<size;i++){
-  cin>>a;
-  v.push_back(a);
-}
 
-sort(v.begin(), v.end());
-for(int i=0; i<size; i++){
-  cout<<v[i]<<" ";
-} 
-return 0;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    //cin and cout
+    vector<int>v;
+    int size;
+    cin>>size;
+    int x;
+    for(int i=0; i<size; i++){
+        //must read the input from STDIN first
+        cin>>x;
+        //the push the integer to a vector
+        v.push_back(x);
+    }
+    //print the sorted order
+    //sort first
+    sort(v.begin(),v.end());
+    for(int j=0; j<size; j++){
+        cout<<v[j]<<" ";
+    }
+    return 0;
 }
