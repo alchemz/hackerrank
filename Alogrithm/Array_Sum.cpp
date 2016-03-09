7013668c3561febc933038644f9ab3f3e391aeb7
xianling zhang <xianlingzhang6@gmail.com>
2016-03-08 23:51:03 -0500

    Create Array_Sum.cpp

---
+++ Alogrithm/Array_Sum.cpp
@@ -0,0 +1,13 @@
+int main() { 
+int i, arr[50], sum, num; 
+scanf("%d", &num);
+for (i = 0; i < num; i++) { 
+scanf("%d", &arr[i]);
+} 
+
+sum = 0; 
+for (i = 0; i < num; i++) { 
+sum = sum + arr[i]; 
+}
+printf("%d", sum);
+return (0); }
