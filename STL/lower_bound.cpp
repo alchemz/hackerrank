578d44595919baaba45b56e420cb53e8aeb77645
xianling zhang <xianlingzhang6@gmail.com>
2016-03-08 19:14:01 -0500

    Update lower_bound.cpp

--- STL/lower_bound.cpp
+++ STL/lower_bound.cpp
@@ -1,12 +1,22 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    return 0;
+  ios::sync_with_stdio(false);
+  int n, q, m;
+  cin >> n;
+  vector<int> v(n);
+  for(int i = 0; i < n; ++i)
+    cin >> v[i];
+  cin >> q;
+  for(int i = 0; i < q; ++i) {
+    cin >> m;
+    if(binary_search(v.begin(), v.end(), m)) {
+      cout << "Yes " << find(v.begin(), v.end(), m) - v.begin() + 1;
+    } else {
+      cout << "No " << lower_bound(v.begin(), v.end(), m) - v.begin() + 1;
+    }
+    cout << "\n";
+  }
+  return 0;
 }
